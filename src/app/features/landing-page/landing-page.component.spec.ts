0d32f88ed4a09a64ef9aa228aab45a67c412db74
VargaDavid21 <davidvarga210220@gmail.com>
2023-08-25 12:13:53 +0100

    Added tests

--- src/app/features/landing-page/landing-page.component.spec.ts
+++ src/app/features/landing-page/landing-page.component.spec.ts
@@ -1,15 +1,42 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { LandingPageComponent } from './landing-page.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {LandingPageComponent} from './landing-page.component';
+import {AuthService} from '../../services/auth/auth.service';
+import {Router} from '@angular/router';
+import {of} from 'rxjs';
+import {By} from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+  let authServiceMock: any;
+  let routerMock: any;
 
   beforeEach(() => {
+    authServiceMock = {
+      getUser: jasmine.createSpy('getUser').and.returnValue(of(null)),
+      signInWithGoogle: jasmine.createSpy('signInWithGoogle').and.returnValue(Promise.resolve())
+    };
+
+    routerMock = {
+      navigate: jasmine.createSpy('navigate')
+    };
+
     TestBed.configureTestingModule({
-      declarations: [LandingPageComponent]
-    });
+      declarations: [LandingPageComponent],
+      imports: [
+        MatCardModule,
+        MatButtonModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: AuthService, useValue: authServiceMock},
+        {provide: Router, useValue: routerMock}
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +44,16 @@ describe('LandingPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should sign in with Google when sign-in button is clicked', () => {
+    const signInButton = fixture.debugElement.query(By.css('.sign-in')).nativeElement;
+    signInButton.click();
+    expect(authServiceMock.signInWithGoogle).toHaveBeenCalled();
+  });
+
+  it('should navigate to search-page after successful sign-in', async () => {
+    await component.signIn();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/search-page']);
   });
 });
--- src/app/features/search-page/search-page.component.spec.ts
+++ src/app/features/search-page/search-page.component.spec.ts
@@ -1,21 +1,71 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { SearchPageComponent } from './search-page.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {SearchPageComponent} from './search-page.component';
+import {NcbiService} from '../../services/ncbi/ncbi.service';
+import {AuthService} from '../../services/auth/auth.service';
+import {Router} from '@angular/router';
+import {AngularFireAuth} from '@angular/fire/compat/auth';
+import {of} from 'rxjs';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('SearchPageComponent', () => {
   let component: SearchPageComponent;
   let fixture: ComponentFixture<SearchPageComponent>;
+  let ncbiService: NcbiService;
+  let authService: AuthService;
+  let router: Router;
+  let afAuth: AngularFireAuth;
 
   beforeEach(() => {
+    const ncbiServiceStub = { getSummaryById: jasmine.createSpy('getSummaryById').and.returnValue(of([])) };
+    const authServiceStub = {}; // Add methods if needed
+    const routerStub = { navigate: jasmine.createSpy('navigate') };
+    const afAuthStub = { signOut: jasmine.createSpy('signOut').and.returnValue(Promise.resolve()) };
+
     TestBed.configureTestingModule({
-      declarations: [SearchPageComponent]
-    });
+      imports: [HttpClientTestingModule],
+      declarations: [SearchPageComponent],
+      providers: [
+        { provide: NcbiService, useValue: ncbiServiceStub },
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: Router, useValue: routerStub },
+        { provide: AngularFireAuth, useValue: afAuthStub }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(SearchPageComponent);
     component = fixture.componentInstance;
+    ncbiService = TestBed.inject(NcbiService);
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
+    afAuth = TestBed.inject(AngularFireAuth);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should log out the user and navigate to landing page', () => {
+    component.logout();
+    expect(afAuth.signOut).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['/landing-page']);
+  });
+
+  it('should call the ncbiService.getSummaryById method with entrezNumber', () => {
+    component.entrezNumber = 'test-number';
+    component.search();
+    expect(ncbiService.getSummaryById).toHaveBeenCalledWith('test-number');
+  });
+
+  it('should return true if numbers are duplicated', () => {
+    component.entrezNumber = '1 1 2';
+    expect(component.isSearchDisabled()).toBeTrue();
+    expect(component.tooltipMessage).toEqual('Please enter each number only once.');
+  });
+
+  it('should return false if numbers are unique', () => {
+    component.entrezNumber = '1 2 3';
+    expect(component.isSearchDisabled()).toBeFalse();
+    expect(component.tooltipMessage).toEqual('');
   });
 });
